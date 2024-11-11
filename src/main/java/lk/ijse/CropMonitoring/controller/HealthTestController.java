9b7b1cf1acf17bb93bdcfab1b54fcc7f13a54afe
panangaladn <dunupa@gmail.com>
2024-11-11 20:31:40 +0530

    frist commit

---
+++ src/main/java/lk/ijse/CropMonitoring/controller/HealthTestController.java
@@ -0,0 +1,18 @@
+package lk.ijse.CropMonitoring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/healthtest")
+public class HealthTestController {
+    @GetMapping
+    public String healthTest(){
+        return "Crop Monitoring run successfully";
+    }
+
+}
+
+
---
+++ src/main/java/lk/ijse/CropMonitoring/controller/Test.java
@@ -0,0 +1,4 @@
+package lk.ijse.CropMonitoring.controller;
+
+public class Test {
+}
