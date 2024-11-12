ac4a00fff3decb928dbe2ef84d1bec648b7bdb7a
panangaladn <dunupa@gmail.com>
2024-11-12 09:02:29 +0530

    create crop entity

---
+++ src/main/java/lk/ijse/CropMonitoring/entity/CropEntity.java
@@ -0,0 +1,35 @@
+package lk.ijse.CropMonitoring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "crops")
+@Entity
+public class CropEntity {
+    @Id
+    @Column(name = "crop_code")
+    private String cropCode;
+    @Column(name = "crop_common_name")
+    private String cropCommonName;
+    @Column(name = "crop_scientific_name")
+    private String cropScientificName;
+    @Column(name = "crop_image", columnDefinition = "LONGTEXT")
+    private String cropImage;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "crop_season")
+    private String cropSeason;
+
+    @ManyToOne
+    @JoinColumn(name = "field_code", referencedColumnName = "field_code")
+    private FieldEntity field;
+
+    @OneToMany(mappedBy = "crop", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CropLogDetailsEntity> cropLogDetailsList = new ArrayList<>();
+
+}
